7e25d3d1889df0a1c79a3b3fd9993b37b5fd0cd5
LuisTul <luis360123@gmail.com>
2023-06-18 18:17:31 -0600

    Se guardo 04

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -11,10 +11,10 @@ const CrearNuevaLinea = (nombre, email, id)=>{
             <ul class="table__button-control">
                 <li>
                     <a
-                        href="../screens/editar_cliente.html"
-                        class="simple-button simple-button--edit"
-                    >Editar</a
-                    >
+                        href="../screens/editar_cliente.html?id=${id}"
+                        class="simple-button simple-button--edit">
+                        Editar
+                    </a>
                 </li>
                 <li>
                     <button class="simple-button simple-button--delete" type="button" id="${id}">
