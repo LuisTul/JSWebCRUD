0e3a68d2fea2d1d366e13474e4683941e76a892c
LuisTul <luis360123@gmail.com>
2023-06-19 14:46:31 -0600

    Se modifico 05

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -1,7 +1,8 @@
 import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector('[data-form]');
-const obtenerInformacion = () =>{
+
+const obtenerInformacion = async() =>{
     const url = new URL(window.location);
     const id = url.searchParams.get('id');
     if(id === null){
@@ -9,12 +10,21 @@ const obtenerInformacion = () =>{
     }
     const nombre = document.querySelector('[data-nombre]');
     const email = document.querySelector('[data-email]');
-    console.log(nombre, " --- ",email);
-    clientServices.detalleCliente(id).then((perfil) =>{
-        nombre.value = perfil.nombre;
-        email.value = perfil.email;
-    });
-}
+
+    try {
+        const perfil = await clientServices.detalleCliente(id);
+        if(perfil.nombre && perfil.email){
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        }else{
+            throw new Error();
+        }
+        
+    } catch (error) {
+        window.location.href = "/screens/error.html"; 
+    }    
+    };
+
 obtenerInformacion();
 formulario.addEventListener("submit",(evento)=>{
     evento.preventDefault();
